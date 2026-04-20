594bda09b607803ac5b641f586dff30c7d3b1b5c
Natalia Portillo <claunia@claunia.com>
2026-04-20 23:55:52 +0100

    Add licensing information to kencode.c and kencode.h

--- kencode.c
+++ kencode.c
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the Aaru Data Preservation Suite.
+ * Copyright (c) 2019-2026 Natalia Portillo.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 // KenCode decompressor
 // Reverse-engineered from Apple DiskCopy 6.3.3 resource fork hdi2 ID 128
 // Algorithm: LZSS with MSB-first bitstream and variable-length prefix codes
